ce8094a3afa64b88c1ea565cfff0dffddd5e8efd
arbabzadehmh <arbabzadeh.mh@gmail.com>
2024-08-05 20:06:38 +0330

    arbabzadeh

---
+++ src/main/java/com/example/cinema_test/controller/exception/DuplicateManagerException.java
@@ -0,0 +1,8 @@
+package com.example.cinema_test.controller.exception;
+
+public class DuplicateManagerException extends Exception{
+    public DuplicateManagerException() {
+        super("Duplicate Manager !!!");
+    }
+}
+
